cfce1b3130b5f0a0dda43df77697b39eb50be2e1
Jansen Rodriguez <JansenR125@gmail.com>
2021-01-28 17:02:59 -0500

    Refactored the aspects by placing each in its own class

--- com/luv2code/aopdemo/aspect/MyApiAnalyticsAspect.java
+++ com/luv2code/aopdemo/aspect/MyApiAnalyticsAspect.java
@@ -2,15 +2,14 @@ package com.luv2code.aopdemo.aspect;
 
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
-import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
 
 @Aspect
 @Component
-public class MyDemoLoggingAspect {
+public class MyApiAnalyticsAspect {
 	
 	@Before("com.luv2code.aopdemo.aspect.LuvAopExpressions.forDaoPackageNoGetterSetter()")
-	public void beforeAddAccountAdvice() {
-		System.out.println("\n==========>> Executing @Before advice on method");
+	public void performApiAnalytics() {
+		System.out.println("\n==========>> Performing API analytics");
 	}
 }
